57f409c3cbb082502b987dc6caf06016e785fd71
MohdSiddiqueBagwan <mohdsiddique.bagwan@gslab.com>
2019-04-08 19:09:53 +0530

    updated for release

--- sonde-release-artifacts.sh
+++ sonde-release-artifacts.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+ARTIFACTS_BUCKET=$(cat sonde-thundra-lambda-warmup-cloudformation-template.yaml | yq '.Resources.ThundraLambdaWarmupFunction.Properties.Code.S3Bucket' -r)
+RELEASE_PATH=$(cat sonde-thundra-lambda-warmup-cloudformation-template.yaml | yq '.Resources.ThundraLambdaWarmupFunction.Properties.Code.S3Key' -r)
+
 mvn clean install
+
 if [ $? -eq 0 ]; then 
-    aws s3 cp target/thundra-lambda-warmup-* s3://sonde-release-artifacts/thundra-lambda-warmup/1.0.0/
-    aws s3 cp sonde-thundra-lambda-warmup-cloudformation-template.yaml s3://sonde-release-artifacts/thundra-lambda-warmup/1.0.0/thundra-lambda-warmup-cloudformation-template.yaml
+    echo "Release Bucket: ${ARTIFACTS_BUCKET}"
+    echo "Release Path: ${RELEASE_PATH}"
+    aws s3 cp target/thundra-lambda-warmup-* s3://${ARTIFACTS_BUCKET}/${RELEASE_PATH}/
+    aws s3 cp sonde-thundra-lambda-warmup-cloudformation-template.yaml s3://${ARTIFACTS_BUCKET}/${RELEASE_PATH}/
     if [ $? -eq 0 ]; then
          echo "thundra lambda is uploaded to sonde-release-artifacts"
     fi
